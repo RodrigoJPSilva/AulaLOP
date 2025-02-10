8fd8d670fb5f071ae97d59593b887b7f12d8b8ae
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-10 11:03:53 -0300

    Exercicios
    Códigos realizados em aula na Escola SENAI.

---
+++ Troco.java
@@ -0,0 +1,25 @@
+package MédiaAltura;
+
+import java.util.Scanner;
+
+public class Troco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); 
+		Double Price, Money, Change;
+		int Unit
+		System.out.print("Preço unitário do produto: ");
+		Price = sc.nextDouble();
+		System.out.print("Quantidade comprada: ");
+		Unit = sc.nextInt();
+		System.out.print("Dinheiro recebido: ");
+		Money = sc.nextDouble();
+		
+		Change = Money - Price * Unit;
+		
+		System.out.print("TROCO = ");
+		System.out.println(String.format("%.2f", Change));
+	}
+
+}
