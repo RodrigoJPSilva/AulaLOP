ea149055d70841c984b8193ffc249b91af810cd0
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-23 19:59:29 -0300

    Update LitrosPorMinuto.java

--- LitrosPorMinuto.java
+++ LitrosPorMinuto.java
@@ -6,7 +6,7 @@ public class LitrosPorMinuto {
 	public static void main(String[] args) {
 	//Variáveis
 	Scanner sc = new Scanner(System.in);
-	double Velocidade, Horas, Distancia,Litros_Usados;
+	double Velocidade, HorasDouble, Distancia,Litros_Usados;
 	int Tempo;
 	//Entrada
 	System.out.print("Seja bem vindo caro usuario");
@@ -16,19 +16,20 @@ public class LitrosPorMinuto {
 	Velocidade = sc.nextDouble();
 	System.out.print("Agora digite os minutos percorridos: ");
 	Tempo = sc.nextInt();
-	Horas = Tempo / 60;
-	
-	//Processamento
-	Distancia = Horas * Velocidade;
-	Litros_Usados = Distancia / 12;	
+    //processamento	
+	HorasDouble = Tempo / 60.0;//convertendo Tempo para double
+	Distancia = HorasDouble * Velocidade;
+	Litros_Usados = Distancia / 12;
+	int HrsInt = Tempo / 60;
+	int MinInt = Tempo % 60;
 	//Saída
 	System.out.printf("Velocidade média: %.0fKM\n", Velocidade);
 	if(Tempo >= 60) {
-		if (Horas == 1){
-			System.out.println("Tempo gasto: " + Horas +" Hora e " + Tempo % 60 + " Minutos");
+		if (HrsInt == 1){
+			System.out.printf("Tempo gasto: %d Hora e %d Minutos\n",HrsInt,MinInt);
 		}
 		else {
-			System.out.println("Tempo gasto: " + Horas +" Horas e " + Tempo % 60 + " Minutos ");
+			System.out.printf("Tempo gasto: %.0f Horas e %d Minutos\n",HrsInt,MinInt);
 		}
 	}
 	else {
