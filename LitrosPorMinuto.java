92c1c7ffbdc23c43d40ddd71a63e31aea6f39a54
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-23 19:16:26 -0300

    Update LitrosPorMinuto.java

--- LitrosPorMinuto.java
+++ LitrosPorMinuto.java
@@ -6,38 +6,37 @@ public class LitrosPorMinuto {
 	public static void main(String[] args) {
 	//Variáveis
 	Scanner sc = new Scanner(System.in);
-	double VELOCIDADE, HORAS, DISTANCIA,LITROS_USADOS;
-	int TEMPO;
+	double Velocidade, Horas, Distancia,Litros_Usados;
+	int Tempo;
 	//Entrada
 	System.out.print("Seja bem vindo caro usuario");
 	System.out.println("Vamos calcular a quantidade de litros gastos na viagem de sua escolha");
 	System.out.println("Um breve aviso: este codigo aceita somente numeros, agora avisado vamos adiante");
 	System.out.print("Digite a velocidade media: ");
-	VELOCIDADE = sc.nextDouble();
+	Velocidade = sc.nextDouble();
 	System.out.print("Agora digite os minutos percorridos: ");
-	TEMPO = sc.nextInt();
-	HORAS = TEMPO / 60;
+	Tempo = sc.nextInt();
+	Horas = Tempo / 60;
 	
 	//Processamento
-	DISTANCIA = HORAS * VELOCIDADE;
-	LITROS_USADOS = DISTANCIA / 12;
-	
+	Distancia = Horas * Velocidade;
+	Litros_Usados = Distancia / 12;	
 	//Saída
-	System.out.printf("Velocidade média: %.0fKM\n", VELOCIDADE);
-	if(TEMPO >= 60) {
-		if (HORAS == 1){
-			System.out.println("Tempo gasto: " + HORAS +" Hora e " + TEMPO % 60 + " Minutos");
+	System.out.printf("Velocidade média: %.0fKM\n", Velocidade);
+	if(Tempo >= 60) {
+		if (Horas == 1){
+			System.out.println("Tempo gasto: " + Horas +" Hora e " + Tempo % 60 + " Minutos");
 		}
 		else {
-			System.out.println("Tempo gasto: " + HORAS +" Horas e " + TEMPO % 60 + " Minutos ");
+			System.out.println("Tempo gasto: " + Horas +" Horas e " + Tempo % 60 + " Minutos ");
 		}
 	}
 	else {
-	System.out.println("Tempo gasto: " + TEMPO + " Minutos");
+	System.out.println("Tempo gasto: " + Tempo + " Minutos");
 	}
-	System.out.printf("Distância percorrida: %.0fKM\n", DISTANCIA);
-	System.out.printf("Litros gastos: %.0f Litros\n", LITROS_USADOS);
-	System.out.print("Agradecemos a sua preferência, volte sempre :)");
+	System.out.printf("Distância percorrida: %.0fKM\n", Distancia);
+	System.out.printf("Litros gastos: %.0f Litros\n", Litros_Usados);
+	System.out.print("Agradecemos a sua preferencia, volte sempre :)");
 	sc.close();
 	}
 }
