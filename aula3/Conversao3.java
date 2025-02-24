729d07ddb36ed12ac506ba2dd1c36162122a234b
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:53:38 -0300

    Update Conversao3.java

--- aula3/Conversao3.java
+++ aula3/Conversao3.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class Conversao3{
 
 			public static void main(String[] args) {
-				//Abrindo o Scanner
+				// Abrindo o Scanner
 				Scanner sc  = new Scanner(System.in);
-				//Variaveis
+				// Variaveis
 				double Volume, R, Altura;
 				
-				//entrada
+				// Entrada
 				System.out.println("Vamos calcular o volume");
 				System.out.println("Digite o raio: ");
 				R = sc.nextDouble();
 				System.out.println("Digite a altura: ");
 				Altura = sc.nextDouble();
 				
-				//processamento
+				// Processamento
 				Volume = 3.14159 * (Math.pow(R,2)) * Altura;
 				
-				//saida
+				// Saida
 				System.out.printf("Em Fehrenheit são %.2fdm³\n", Volume);
 
-				//Fechando o Scanner
+				// Fechando o Scanner
 				sc.close();
 
 			}
