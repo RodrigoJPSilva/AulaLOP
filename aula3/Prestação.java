968289d1a72c03df50c946b86455021eb7cc628f
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:59:43 -0300

    Update Prestação.java

--- aula3/Prestação.java
+++ aula3/Prestação.java
@@ -8,24 +8,24 @@ public class Prestação {
 		// Abrindo o Scanner
 		Scanner sc  = new Scanner(System.in);
 		// Variaveis
-		double Prestacao, Valor, Taxa, Tempo;
+		double prestacao, valor, taxa, tempo;
 
 		// entrada
 		System.out.println("Seja bem vindo, caro usuario");
 		System.out.println("Neste codigo, sera possivel verificar quanto voce pagara de juros em uma prestacao");
 		System.out.println("AVISO: este codigo aceita somente numeros");
 		System.out.print("Digite o valor da prestacao: ");
-		Valor = sc.nextDouble();
+		valor = sc.nextDouble();
 		System.out.print("Agora digite a taxa da prestacao(porcentagem a ser cobrada): ");
-		Taxa = sc.nextDouble();
+		taxa = sc.nextDouble();
 		System.out.print("Agora digite o tempo em anos da prestacao: ");
-		Tempo = sc.nextDouble();
+		tempo = sc.nextDouble();
 
 		// processamento
-		Prestacao = Valor + (Valor * (Taxa / 100) * Tempo);
+		prestacao = valor + (valor * (taxa / 100) * tempo);
 
 		// saida
-		System.out.printf("A sua prestacao sera de %.2f reais", Prestacao);
+		System.out.printf("A sua prestacao sera de %.2f reais", prestacao);
 		// Fechando o Scanner
 		sc.close();
 	}
