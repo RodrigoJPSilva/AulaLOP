7194f97dc8bd4e17688cf2dec4bf696734c00a64
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:54:30 -0300

    Update ValoresElevados.java

--- aula2/ValoresElevados.java
+++ aula2/ValoresElevados.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class ValoresElevados {
 
 	public static void main(String[] args) {
-		//Criando o Scanner
+		// Criando o Scanner
 		Scanner sc  = new Scanner(System.in);
-		//Variaveis
+		// Variaveis
 		Double Height, Width, Area, Price, Total;
 
-		//Entrada
+		// Entrada
 		System.out.print("Digite a largura do cômodo: ");
 		Height = sc.nextDouble();
 		System.out.print("Digite o comprimento do comodo: ");
@@ -18,16 +18,16 @@ public class ValoresElevados {
 		System.out.print("Digite o valor do metro quadrado do piso: ");
 		Price = sc.nextDouble();
 		
-		//Processamento
+		// Processamento
 		Area = Height * Width;
 		Total = Price * Area;
 
-		//Saida
+		// Saida
 		System.out.println("O valor em metros quadrados são: " + Area);
 		System.out.print("O valor do seu produto é: ");
 		System.out.print(String.format("%.2f", Total));
 
-		//Fechando o Scanner
+		// Fechando o Scanner
 		sc.close();
 
 	}
