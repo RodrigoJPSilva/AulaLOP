b54955f544610cc0227b0c709a6bf74747ac2a82
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 19:33:19 -0300

    Update Troco.java

--- aula2/Troco.java
+++ aula2/Troco.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class Troco {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in); 
+		//Criando o Scanner
+		Scanner sc  = new Scanner(System.in);
+		//Variaveis
 		Double Price, Money, Change;
 		int Unit
+
+		//Entrada
 		System.out.print("Preço unitário do produto: ");
 		Price = sc.nextDouble();
 		System.out.print("Quantidade comprada: ");
 		Unit = sc.nextInt();
 		System.out.print("Dinheiro recebido: ");
 		Money = sc.nextDouble();
-		
+
+		//Processamento
 		Change = Money - Price * Unit;
-		
+
+		//Saida
 		System.out.print("TROCO = ");
 		System.out.println(String.format("%.2f", Change));
 	}
