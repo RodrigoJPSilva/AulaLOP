1de56a325799b8b8fbb60372ada5d91ac58733b0
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:50:18 -0300

    Update AnoBissexto.java

--- aula4/AnoBissexto.java
+++ aula4/AnoBissexto.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class AnoBissexto {
 
 	public static void main(String[] args) {
-		//Abrindo o Scanner
+		// Abrindo o Scanner
 		Scanner sc = new Scanner(System.in);
-		//Variaveis
+		// Variaveis
 		int ano;
 		System.out.printf("Seja bem vindo caro usuario\nVenha testar meu codigo e descobrir um ano bissexto\nAVISO:Esse codigo aceita somente numeros\n");
 		System.out.printf("Insira o ano:");
 		ano = sc.nextInt();
 	    
-		//Saida Condicional
+		// Saida Condicional
 		if ((ano % 4 == 0 && ano % 100 != 0)||(ano % 400 == 0)) {
 			System.out.printf("O ano %d e bissexto",ano);
 		}
 		else {
 			System.out.printf("O ano %d nao e bissexto",ano);			
 		}
-		//Fechando o Scanner
+		// Fechando o Scanner
 		sc.close();
 	}
 
