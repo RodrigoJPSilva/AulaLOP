ef5cd7afd1867fc177ccbb28c6fce699ba99a10b
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 19:09:24 -0300

    Add files via upload

---
+++ aula2/Quadro.java
@@ -0,0 +1,40 @@
+package PrimeiraAula;
+
+import java.util.Scanner;
+
+public class Quadro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		Double Height, Width, Area, Perimeter, Diagonal;
+
+		System.out.print("Largura do comodo: ");
+		Height = sc.nextDouble();
+
+		System.out.print("Altura do Quadro: ");
+		Width = sc.nextDouble();
+		
+
+		Area = Height * Width;
+		
+
+		System.out.println("O valor em metros quadrados são: " + Area);
+		
+		System.out.print("ÁREA = ");
+		System.out.println(String.format("%.4f", Area));
+		Perimeter = 2 * (Height+Width);
+		
+		System.out.print("PEÍMETRO = ");
+		System.out.println(String.format("%.4f", Perimeter));
+		Diagonal = Math.sqrt(Height * Height+Width * Width); 
+		
+		System.out.print("DIAGONAL = ");
+		System.out.println(String.format("%.4f", Diagonal));
+		
+		sc.close();
+
+	}
+
+}
