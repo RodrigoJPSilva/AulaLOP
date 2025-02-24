e9ca6e08eeb193fe94bf61fcc4f0cb5d5b116d35
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 19:38:16 -0300

    Update ValoresElevados.java

--- aula2/ValoresElevados.java
+++ aula2/ValoresElevados.java
@@ -10,28 +10,24 @@ public class ValoresElevados {
 		//Variaveis
 		Double Height, Width, Area, Price, Total;
 
-		
+		//Entrada
 		System.out.print("Digite a largura do cômodo: ");
 		Height = sc.nextDouble();
-
 		System.out.print("Digite o comprimento do comodo: ");
 		Width = sc.nextDouble();
-		
 		System.out.print("Digite o valor do metro quadrado do piso: ");
 		Price = sc.nextDouble();
 		
-
+		//Processamento
 		Area = Height * Width;
-		
-
-		System.out.println("O valor em metros quadrados são: " + Area);
-		
 		Total = Price * Area;
-		
+
+		//Saida
+		System.out.println("O valor em metros quadrados são: " + Area);
 		System.out.print("O valor do seu produto é: ");
 		System.out.print(String.format("%.2f", Total));
-		
 
+		//Fechando o Scanner
 		sc.close();
 
 	}
