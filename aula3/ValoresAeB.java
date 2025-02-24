f271c4b66410787045e6b4e14f081cceea5b5a99
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:51:11 -0300

    Update ValoresAeB.java

--- aula3/ValoresAeB.java
+++ aula3/ValoresAeB.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class ValoresAeB {
 	public static void main(String[] args) {
-		//Abrindo o Scanner
+		// Abrindo o Scanner
 		Scanner sc = new Scanner(System.in);
-		//Variaveis
+		// Variaveis
 		String A, B, C;
 
-		//Entrada
+		// Entrada
 		System.out.println("Seja bem vindo caro usuario");
 		System.out.println("Neste codigo, voce podera colocar dois caracteres, e eles serao trocados de ordem pelo codigo");
 
-		//String A
+		// String A
 		System.out.print("Coloque o primeiro caractere: ");
 		A = sc.nextLine();
-		//String B
+		// String B
 		System.out.print("Coloque o segundo caractere: ");
 		B = sc.nextLine();
 
-		//Saida normal
+		// Saida normal
 		System.out.printf("Primeiro caractere: %s\n Segundo caractere: %s\n",A,B);
 		System.out.println("Agora vamos trocar os valores de lugar");
-		//Processamento
+		// Processamento
 		C = A;
 		A = B;
 		B = C;
-		//Saida apos a troca
+		// Saida apos a troca
 		System.out.printf("Primeiro caractere: %s\n Segundo caractere: %s\n",A,B);
 		sc.close();
 	}
