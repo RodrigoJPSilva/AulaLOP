c6ef0ec86e3ccdc3fbed049a6791d11dd715cdc0
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:49:51 -0300

    Update TesteAprovacao.java

--- aula4/TesteAprovacao.java
+++ aula4/TesteAprovacao.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class TesteAprovacao {
 
 	public static void main(String[] args) {
-		//Abrindo o Scanner
+		// Abrindo o Scanner
 		Scanner sc = new Scanner(System.in);
-		//Variaveis
+		// Variaveis
 		double nota;
 		String nome;
 
-		//Entrada
+		// Entrada
 		System.out.println("Seja bem vindo caro usuario");
 		System.out.printf("AVISO:Este codigo aceita somente numeros\nVamos ver se voce passou no texte\ninsira o seu nome: ");
 	        nome = sc.nextLine();
 	        System.out.printf("Agora insira a sua nota: \n");
 		nota = sc.nextDouble();
 
-		//Saida Condicional
+		// Saida Condicional
 	        if (nota >= 7) {
 		    	System.out.printf("O aluno %s tirou %.2f", nome, nota);
 		    	System.out.print(" Aprovado");
@@ -28,7 +28,7 @@ public class TesteAprovacao {
 		    	System.out.print(" Reprovado");
 		    }
 		
-		//Fechando o Scanner
+		// Fechando o Scanner
 		sc.close();
 		}
 	}
