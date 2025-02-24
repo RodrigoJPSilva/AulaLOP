6a58156e7c3a4623af9c27946494916c80b5b7c9
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:53:54 -0300

    Update Conversao2.java

--- aula3/Conversao2.java
+++ aula3/Conversao2.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class Conversao2{
 
 			public static void main(String[] args) {
-				//Abrindo o Scanner
+				// Abrindo o Scanner
 				Scanner sc  = new Scanner(System.in);
-				//Variaveis
+				// Variaveis
 				double Fahrenheit,Conversão;
 				
-				//entrada
+				// Entrada
 				System.out.println("Vamos converter Celsious para Fahrenheit");
 				System.out.println("Digite os graus Celsious: ");
 				Fahrenheit = sc.nextDouble();
 				
-				//processamento
+				// Processamento
 				Conversão = ((Fahrenheit-32)* 5 / 9);
 				
-				//saida
+				// Saida
 				System.out.printf("Em Fehrenheit são %.2f°F\n", Conversão);
 
-				//Fechando o Scanner
+				// Fechando o Scanner
 				sc.close();
 
 			}
