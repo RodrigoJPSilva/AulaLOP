88922f020a8d92f59a5bc86c43c73b067926ed47
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 20:53:07 -0300

    Update IMC.java

--- aula3/IMC.java
+++ aula3/IMC.java
@@ -24,20 +24,15 @@ public class IMC {
 		// Saida
 		if (IMC < 18.5) {
 			System.out.println("Você está abaixo o do peso");
-		}
-		else if(IMC >= 18.5 && IMC <= 24.9) {
+		} else if(IMC >= 18.5 && IMC <= 24.9) {
 			System.out.println("Você está no peso ideal");
-		}
-		else if(IMC >= 25 && IMC <= 29.9) {
+		} else if(IMC >= 25 && IMC <= 29.9) {
 			System.out.println("Você está com sobrepeso");
-		}
-		else if(IMC >= 30 && IMC <= 34.9) {
+		} else if(IMC >= 30 && IMC <= 34.9) {
 			System.out.println("Você está com obesidade grau 1");
-		}
-		else if(IMC >= 35 && IMC <= 39.9) {
+		} else if(IMC >= 35 && IMC <= 39.9) {
 			System.out.println("Você está com obesidade grau 2");
-		}
-		else if(IMC >= 40) {
+		} else if(IMC >= 40) {
 			System.out.println("Você está com obesidade grau 3");
 		}
 
