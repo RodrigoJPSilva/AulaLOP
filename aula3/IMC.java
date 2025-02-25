044f01958c876a9022a033458d0b671b091d8acb
RodrigoJPSilva <rodrigojrpsilva@gmail.com>
2025-02-24 21:03:04 -0300

    Update IMC.java

--- aula3/IMC.java
+++ aula3/IMC.java
@@ -8,31 +8,31 @@ public class IMC {
 		// Abrindo o Scanner
 		Scanner sc  = new Scanner(System.in);
 		// Variaveis
-		double Peso,Altura, IMC;
+		double peso,altura, imc;
 		
 		// Entrada
 		System.out.println("Vamos ver o seu Indíce de Massa Muscular ");
 		System.out.println("Digite o seu peso: ");
-		Peso = sc.nextDouble();
+		peso = sc.nextDouble();
 		System.out.println("Digite sua altura: ");
-		Altura = sc.nextDouble();
+		altura = sc.nextDouble();
 		
 		// Processamento
-		IMC = Peso / (Math.pow(Altura, 2));
-		System.out.printf("IMC = %.2f\n", IMC);
+		imc = peso / (Math.pow(altura, 2));
+		System.out.printf("IMC = %.2f\n", imc);
 		
 		// Saida
-		if (IMC < 18.5) {
+		if (imc < 18.5) {
 			System.out.println("Você está abaixo o do peso");
-		} else if(IMC >= 18.5 && IMC <= 24.9) {
+		} else if(imc >= 18.5 && imc <= 24.9) {
 			System.out.println("Você está no peso ideal");
-		} else if(IMC >= 25 && IMC <= 29.9) {
+		} else if(imc >= 25 && imc <= 29.9) {
 			System.out.println("Você está com sobrepeso");
-		} else if(IMC >= 30 && IMC <= 34.9) {
+		} else if(imc >= 30 && imc <= 34.9) {
 			System.out.println("Você está com obesidade grau 1");
-		} else if(IMC >= 35 && IMC <= 39.9) {
+		} else if(imc >= 35 && imc <= 39.9) {
 			System.out.println("Você está com obesidade grau 2");
-		} else if(IMC >= 40) {
+		} else if(imc >= 40) {
 			System.out.println("Você está com obesidade grau 3");
 		}
 
