162d4fa18a18c5a203983ef1a197584b7a5a8ba4
Rodrigo Silva <rodrigojrpsilva@gmail.com>
2025-05-12 10:12:01 -0300

    Update Desafio1.java

--- aula9Desafio/Desafio1.java
+++ aula9Desafio/Desafio1.java
@@ -1,4 +1,4 @@
-package aula9Desafio;
+package Desafios2;
 
 import java.util.Scanner;
 
@@ -6,53 +6,48 @@ public class situacaoDesafioTeste {
 
 	public static void main(String[] args) {
 
-		/* Vetor [0] Salário bruto, vetor [1] INSS, vetor [9] Salário liquido,
-		vetor [2] Imposto de renda, vetor [3] Dependentes, vetor [4] vale alimentação,
-		vetor [5] Vale alimentação, vetor [6] Vale refeição,
-		vetor [7] Plano de saúde basico, vetor [8] Plano de saúde avançado */
+		/*
+		 * Vetor [0] Salário bruto, vetor [1] INSS, vetor [9] Salário liquido, vetor [2]
+		 * Imposto de renda, vetor [3] Dependentes, vetor [4] vale alimentação, vetor
+		 * [5] Vale alimentação, vetor [6] Vale refeição, vetor [7] Plano de saúde
+		 * basico, vetor [8] Plano de saúde avançado
+		 */
 		// Variáveis
 
 		Scanner sc = new Scanner(System.in);
 		double[] vetor = new double[10];
-		String[] opcoes = {"Salário bruto: ","Salário liquido: ", "Imposto de renda: -", "Dependentes: ", "Vale alimentação: -",
-				"Vale refeição: -", "Plano de saúde básico: -", "Plano de saúde Avançado: -"}; 
+		String[] opcoes = { "Salário bruto: ", "INSS: -", "Imposto de renda: -", "Dependentes: ", "Vale transporte: -",
+				"Desconto Vale alimentação: -", "Desconto Vale refeição: -", "Desconto Plano de saúde básico: -",
+				"Desconto Plano de saúde Avançado: -", "Total/Salário Liquido: " };
 
 		// Entrada
 
 		System.out.println(
-				"Seja bem vindo, nesse prrograma vamos calcular\n" + "o salário bruto e subtrair dos descontos");
+				"Seja bem vindo, nesse programa vamos calcular\n" + "o salário bruto e subtrair dos descontos");
 		System.out.print("Insira o salário bruto: R$ ");
 		// vetor[0] = salário bruto
 		vetor[0] = sc.nextDouble();
 
 		// Processamento
-		
-		// Aplicando o desconto do INSS
+
 		// vetor[1] = inss
 		vetor[1] = 0.00;
 		// vetor[9] = salário líquido
 		vetor[9] = vetor[0];
-		if (vetor[0] <= 1302.00) {
+		// Aplicando o desconto do INSS
+		if (vetor[0] <= 1302.00)
 			vetor[1] += 0.08 * vetor[0];
-			vetor[9] -= vetor[1];
-			System.out.printf("inss = -R$%.2f\n", vetor[1]);
-		} else if (vetor[0] > 1302.00 && vetor[0] <= 2571.00) {
+		else if (vetor[0] > 1302.00 && vetor[0] <= 2571.00)
 			vetor[1] += 0.09 * vetor[0];
-			vetor[9] -= vetor[1];
-			System.out.printf("inss = -R$%.2f\n", vetor[1]);
-		} else if (vetor[0] > 2571.00 && vetor[0] <= 3856.00) {
+		else if (vetor[0] > 2571.00 && vetor[0] <= 3856.00)
 			vetor[1] += 0.11 * vetor[0];
-			vetor[9] -= vetor[1];
-			System.out.printf("inss = -R$%.2f\n", vetor[1]);
-		} else if (vetor[0] > 3856.00 && vetor[0] < 7087.22) {
+		else if (vetor[0] > 3856.00 && vetor[0] < 7087.22)
 			vetor[1] += 0.14 * vetor[0];
-			vetor[9] -= vetor[1];
-			System.out.printf("inss = -R$%.2f\n", vetor[1]);
-		} else {
+		else
 			vetor[1] += 570.88;
-			vetor[9] -= vetor[1];
-			System.out.printf("inss = -R$%.2f\n", vetor[1]);
-		}
+
+		vetor[9] -= vetor[1];
+		System.out.printf("inss = -R$%.2f\n", vetor[1]);
 		// Aplicando o desconto do imposto de renda
 		// vetor[2] = imposto de renda
 		vetor[2] = 0.00;
@@ -70,51 +65,44 @@ public class situacaoDesafioTeste {
 		// Caso com dependentes
 		switch (vetor[3] > 0.00 ? 1 : 2) {
 		case 1:
-			if (vetor[0] <= 1903.98) {
-				System.out.println("Isento do imposto de renda");
-			} else if (vetor[0] > 1903.98 && vetor[0] <= 2826.65) {
-				vetor[2] += ((0.075 * vetor[0]) - 142.80) - (vetor[3] * 189.59);
-				vetor[9] -= vetor[2];
-			} else if (vetor[0] > 2826.65 && vetor[0] <= 3751.05) {
-				vetor[2] += ((0.15 * vetor[0]) - 354.80) - (vetor[3] * 189.59);
-				vetor[9] -= vetor[2];
-			} else if (vetor[0] > 3751.05 && vetor[0] <= 4664.68) {
-				vetor[2] += ((0.22 * vetor[0]) - 636.13) - (vetor[3] * 189.59);
-				vetor[9] -= vetor[2];
-			} else {
-				vetor[2] += ((0.275 * vetor[0]) - 869.36) - (vetor[3] * 189.59);
-				vetor[9] -= vetor[2];
-			}
+			System.out.println("Isento do imposto de renda");
+			if (vetor[0] > 1903.98 && vetor[0] <= 2826.65)
+				vetor[2] += ((0.075 * vetor[0]) + (vetor[3] * -189.59));
+			else if (vetor[0] > 2826.65 && vetor[0] <= 3751.05)
+				vetor[2] += ((0.15 * vetor[0]) - (vetor[3] * -189.59));
+			else if (vetor[0] > 3751.05 && vetor[0] <= 4664.68)
+				vetor[2] += ((0.22 * vetor[0]) - (vetor[3] * -189.59));
+			else
+				vetor[2] += ((0.275 * vetor[0]) - (vetor[3] * -189.59));
+
 			// Verifica se o imposto de renda foi zerado por causa dos dependentes
 			if (vetor[2] <= 0.00) {
-				vetor[2] = 0.00;
 				System.out.println("O imposto de renda foi zerado por causa dos dependentes.");
-			} else {
+				vetor[2] = 0;
+			} else
 				System.out.printf("Imposto de renda = -R$%.2f\n", vetor[2]);
-			}
+			vetor[9] -= vetor[2];
+
 			break;
 		// Caso sem dependentes
 		case 2:
 			if (vetor[0] <= 1903.98) {
-				vetor[9] = vetor[0];
 				System.out.println("Isento do imposto de renda");
+				vetor[2] = 0;
 			} else if (vetor[0] > 1903.98 && vetor[0] <= 2826.65) {
-				vetor[2] += (0.075 * vetor[0]) - 142.80;
-				vetor[9] -= vetor[2];
+				vetor[2] += 0.075 * vetor[0];
 				System.out.printf("imposto de renda = -R$%.2f\n", vetor[2]);
 			} else if (vetor[0] > 2826.65 && vetor[0] <= 3751.05) {
-				vetor[2] += (0.15 * vetor[0]) - 354.80;
-				vetor[9] -= vetor[2];
+				vetor[2] += 0.15 * vetor[0];
 				System.out.printf("imposto de renda = -R$%.2f\n", vetor[2]);
 			} else if (vetor[0] > 3751.05 && vetor[0] <= 4664.68) {
-				vetor[2] += (0.22 * vetor[0]) - 636.13;
-				vetor[9] -= vetor[2];
+				vetor[2] += 0.22 * vetor[0];
 				System.out.printf("imposto de renda = -R$%.2f\n", vetor[2]);
 			} else {
-				vetor[2] += (0.275 * vetor[0]) - 869.36;
-				vetor[9] -= vetor[2];
+				vetor[2] += 0.275 * vetor[0];
 				System.out.printf("imposto de renda = -R$%.2f\n", vetor[2]);
 			}
+			vetor[9] -= vetor[2];
 			break;
 		}
 		// Desconto do vale transporte
@@ -125,9 +113,9 @@ public class situacaoDesafioTeste {
 			try {
 				System.out.println("Recebe vale transporte ?  [s/n]");
 				vt = sc.next().toUpperCase().charAt(0);
-				if (vt == 'S' || vt == 'N') {
+				if (vt == 'S' || vt == 'N')
 					break;
-				} else {
+				else {
 					System.out.println("Entrada inválida, por favor digite apenas sim ou não.");
 				}
 			} catch (Exception e) {
@@ -153,9 +141,9 @@ public class situacaoDesafioTeste {
 			try {
 				System.out.println("Recebe vale alimentação ? [s/n]");
 				va = sc.next().toUpperCase().charAt(0);
-				if (va == 'S' || va == 'N') {
+				if (va == 'S' || va == 'N')
 					break;
-				} else {
+				else {
 					System.out.println("Entrada inválida, por favor digite apenas sim ou não. ");
 				}
 			} catch (Exception e) {
@@ -164,7 +152,7 @@ public class situacaoDesafioTeste {
 		}
 		switch (va) {
 		case 'S':
-			vetor[5] = 200;
+			vetor[5] = vetor[0] * 20 / 100;
 			vetor[9] -= vetor[5];
 			System.out.printf("Desconto do vale alimentação = -R$%.2f\n", vetor[5]);
 			break;
@@ -180,9 +168,9 @@ public class situacaoDesafioTeste {
 		while (true) {
 			try {
 				vr = sc.next().toUpperCase().charAt(0);
-				if (vr == 'S' || vr == 'N') {
+				if (vr == 'S' || vr == 'N')
 					break;
-				} else {
+				else {
 					System.out.println("Entrada inválida, por favor digite apenas sim ou não");
 				}
 			} catch (Exception e) {
@@ -191,7 +179,8 @@ public class situacaoDesafioTeste {
 		}
 		switch (vr) {
 		case 'S':
-			vetor[6] = 250.00;
+			vetor[6] = vetor[0] * 20 / 100;
+			;
 			vetor[9] -= vetor[6];
 			System.out.printf("Desconto do vale refeição = -R$%.2f\n", vetor[6]);
 			break;
@@ -209,11 +198,10 @@ public class situacaoDesafioTeste {
 		while (true) {
 			try {
 				plano = sc.next().toUpperCase().charAt(0);
-				if (plano == 'B' || plano == 'A' || plano == 'N') {
+				if (plano == 'B' || plano == 'A' || plano == 'N')
 					break;
-				} else {
+				else
 					System.out.println("Entrada inválida, por favor digite apenas basico, avançado ou não");
-				}
 			} catch (Exception e) {
 				System.out.println("Ocorreu um erro... tente novamente");
 			}
@@ -234,10 +222,12 @@ public class situacaoDesafioTeste {
 			System.out.println("Desconto do plano de saúde = R$0,00");
 			break;
 		}
-		
+
 		// Saída
 		for (int j = 0; j < opcoes.length; j++) {
-		System.out.printf("Desconto %sR$%.2f\n",opcoes[j],vetor[j]);
+			if (j != 3)
+				System.out.printf("%sR$%.2f\n", opcoes[j], vetor[j]);
 		}
-}
+		sc.close();
+	}
 }
